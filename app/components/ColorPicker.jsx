1237c235afd2c64f0cff280bcb3a45eef8bb777d
TejasBhovad <tejasbhovad@gmail.com>
2023-12-19 21:08:34 +0530

    style: UI added

--- app/components/ColorPicker.jsx
+++ app/components/ColorPicker.jsx
@@ -3,53 +3,25 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 
-export default function Greet() {
-  const [greeting, setGreeting] = useState("");
+export default function ColorPicker({ setShades }) {
   const [color, setColor] = useState("#000000");
-  const [shades, setShades] = useState([]);
-
   useEffect(() => {
-    invoke("greet", { name: "Next.js" })
-      .then((result) => setGreeting(result))
-      .catch(console.error);
-
     invoke("get_shades", { color: color })
       .then((result) => setShades(result))
       .catch(console.error);
   }, [color]);
 
-  // Necessary because we will have to use Greet as a component later.
   return (
-    <div>
-      {greeting}
+    <div className="h-24 flex items-center justify-center rounded-sm bg-gray-00 p-2">
       <input
+        className="w-full h-full rounded-sm bg-transparent"
         type="color"
         value={color}
         onChange={(e) => setColor(e.target.value)}
       />
-
-      {color}
-      {shades}
-      {/* map throught array of colors */}
-      {/* {shades.map((shade) => (
-        <div
-          key={shade}
-          style={{ backgroundColor: shade, width: "100px", height: "100px" }}
-        >
-          {shade}
-        </div>
-      ))} */}
-      {/* while mapping filter all colors with cakue #000000 */}
-      {shades
-        .filter((shade) => shade !== "#000000" && shade !== "#FFFFFF")
-        .map((shade) => (
-          <div
-            key={shade}
-            style={{ backgroundColor: shade, width: "100px", height: "100px" }}
-          >
-            {shade}
-          </div>
-        ))}
+      <span className="absolute" style={{ pointerEvents: "none" }}>
+        {color}
+      </span>
     </div>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,41 @@
+"use client";
 import React from "react";
 import ColorPicker from "@/app/components/ColorPicker";
+import { useToast } from "@/components/ui/use-toast";
+
+import { useState } from "react";
 
 const page = () => {
+  const [shades, setShades] = useState([]);
+  const { toast } = useToast();
+  const copyToClipboard = (color) => {
+    toast({
+      title: `${color}`,
+      description: "Copied to clipboard!",
+    });
+    navigator.clipboard.writeText(color);
+  };
+
   return (
-    <div>
-      page
-      <ColorPicker />
+    <div className="w-full h-full">
+      <div className="w-full h-full px-8 py-2 bg-gray-900 flex flex-col gap-8">
+        <ColorPicker setShades={setShades} className="h-1/4 " />
+        <div className="overflow-y-auto h-3/4 w-full bg-gray-800 p-4">
+          {shades
+            .filter((shade) => shade !== "#000000" && shade !== "#FFFFFF")
+            .map((shade) => (
+              <div
+                key={shade}
+                style={{ backgroundColor: shade }}
+                className="h-24 flex items-center justify-center"
+                onClick={() => copyToClipboard(shade)}
+              >
+                <span>{shade}</span>
+              </div>
+            ))}
+          {/* button o show toast */}
+        </div>
+      </div>
     </div>
   );
 };
